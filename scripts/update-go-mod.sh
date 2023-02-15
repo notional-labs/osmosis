23664b35e286750b293a44d7ac66df36d52553fb
Ruslan Akhtariev <rakhtariev@icloud.com>
2023-02-15 10:50:11 +0700

    wip

--- scripts/update-go-mod.sh
+++ scripts/update-go-mod.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# script parameters, in same order as: osmoutils, osmomath, ibc-hooks
 
-check_update() {
+is_updated() {
     if [ "${1}" != "" ]
     then
         return 1
@@ -9,46 +8,43 @@ check_update() {
     return 0
 }
 
-main_commit=$(git rev-parse origin/main)
+main_commit=$(git rev-parse origin/$GITHUB_BASE_REF)
+head_commit=$(git rev-parse origin/$GITHUB_HEAD_REF)
 
-changed_osmoutils=$(git diff --name-only $main_commit HEAD | grep osmoutils)
-changed_osmomath=$(git diff --name-only $main_commit HEAD | grep osmomath)
-changed_ibc_hooks=$(git diff --name-only $main_commit HEAD | grep x/ibc-hooks)
+changed_osmoutils=$(git diff --name-only $main_commit $head_commit | grep osmoutils)
+changed_osmomath=$(git diff --name-only $main_commit $head_commit | grep osmomath)
+changed_ibc_hooks=$(git diff --name-only $main_commit $head_commit | grep x/ibc-hooks)
 
-check_update $changed_osmoutils
+is_updated $changed_osmoutils
 update_osmoutils=$?
 
-check_update $changed_osmomath
+is_updated $changed_osmomath
 update_osmomath=$?
 
-check_update $changed_ibc_hooks
+is_updated $changed_ibc_hooks
 update_ibc_hooks=$?
 
-latest_commit=$(git rev-parse HEAD)
 any_updated=0 # we do not want to run `go mod tidy`` in case none of these files have changed
 
 if [ $update_osmoutils -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/osmoutils@$latest_commit
+    go get github.com/osmosis-labs/osmosis/osmoutils@$head_commit
     any_updated=1
 fi
 
 if [ $update_osmomath -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/osmomath@$latest_commit
+    go get github.com/osmosis-labs/osmosis/osmomath@$head_commit
     any_updated=1
 fi
 
 if [ $update_ibc_hooks -eq 1 ]
 then 
-    go get github.com/osmosis-labs/osmosis/x/ibc-hooks@$latest_commit
+    go get github.com/osmosis-labs/osmosis/x/ibc-hooks@$head_commit
     any_updated=1
 fi
 
 if [ $any_updated -eq 1 ]
 then
     go mod tidy
-    echo "MAKE_PULL_REQUEST=1" >> $GITHUB_ENV
-    echo running go mod tidy
 fi
-exit
